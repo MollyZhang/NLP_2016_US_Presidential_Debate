af4fdc8359b15e8d583b070a8f2b8350284ef070
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-14 17:53:29 -0700

    finish LSTM script for training trump words

--- LSTM_trumpifiy_text_gen.py
+++ LSTM_trumpifiy_text_gen.py
@@ -43,4 +43,9 @@ model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
+# define the checkpoint
+filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
 
+model.fit(X, y, nb_epoch=20, batch_size=128, callbacks=callbacks_list)
