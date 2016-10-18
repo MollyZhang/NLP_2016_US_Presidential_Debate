2b6043ae5ec2c11d003fa8417d407fa184445fd7
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-18 11:51:24 -0700

    add more two-layer LSTM model

--- LSTM_trumpify.py
+++ LSTM_trumpify.py
@@ -16,8 +16,8 @@ from keras.utils import np_utils
 def main():
     X, y, dataX, chars, n_vocab = load_data("data/debate.csv")
     model = define_model(X, y)
-    #train_model(model, X, y)
-    generate_text(model, dataX, chars, n_vocab)
+    train_model(model, X, y)
+    #generate_text(model, dataX, chars, n_vocab)
 
 
 def generate_text(model, dataX, chars, n_vocab):
@@ -46,11 +46,12 @@ def generate_text(model, dataX, chars, n_vocab):
     print "\nDone."
 
 
- 
 
 def define_model(X, y):
     model = Sequential()
-    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(256))
     model.add(Dropout(0.2))
     model.add(Dense(y.shape[1], activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam')
@@ -89,10 +90,10 @@ def load_data(filename):
 
 def train_model(model, X, y):
     # define the checkpoint
-    filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+    filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
-    model.fit(X, y, nb_epoch=20, batch_size=128, callbacks=callbacks_list)
+    model.fit(X, y, nb_epoch=50, batch_size=64, callbacks=callbacks_list)
 
 
 
